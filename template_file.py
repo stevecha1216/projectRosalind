99f12d0f75305112083fb3ae1a88d2c1d89b00a5
Steve Cha <89746831+stevecha1216@users.noreply.github.com>
2023-02-13 02:48:13 -0600

    Create template_file.py

---
+++ template_file.py
@@ -0,0 +1,19 @@
+import fileinput as fi
+import fileoutput as fo
+
+
+
+
+
+if __name__ == "__main__":
+    #change filename
+    #filename = "_test"
+    filename = "rosalind_"
+
+    #keep this the same
+    datain = fi.readinput(filename)
+
+    #change the function name
+    output = #function(datain)
+    #change the output file name
+    fo.out(output, "")
