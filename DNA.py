e0bac6d716220ce3bb079dd4fc899a6467110722
Steve Cha <89746831+stevecha1216@users.noreply.github.com>
2023-03-21 10:49:03 -0500

    Update DNA.py

--- DNA.py
+++ DNA.py
@@ -1,5 +1,34 @@
+import fileinput as fi
+import fileoutput as fo
+
+def nucCount(s):
+    s = list(s)
+    #print(s)
+    a,t,g,c = 0,0,0,0
+    for nuc in s:
+        if nuc == "A":
+            a += 1
+        elif nuc == "T":
+            t += 1
+        elif nuc == "G":
+            g += 1
+        elif nuc == "C":
+            c += 1
+    out = str(a) + " " + str(c) + " " + str(g) + " " + str(t)
+    #print(out)
+    return(out)
 
 
 
 if __name__ == "__main__":
-    print()
+    #change filename
+    #filename = "dna_test"
+    filename = "rosalind_dna"
+
+    #keep this the same
+    datain = fi.readinput(filename)
+
+    #change the function name
+    output = nucCount(datain)
+    #change the output file name
+    fo.out(output, "DNA")
